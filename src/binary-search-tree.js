4cd3341e20c289176fa58b9f84ae0635c36392dc
Turseya <turseya@gmail.com>
2021-10-18 20:20:41 +0300

    init: add binary search three

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -65,10 +65,41 @@ module.exports = class BinarySearchTree {
   }
 
   remove(data) {
-
+    this.base = this.removeNode(this.base, data)
   }
-
-
+  removeNode(node, data) {
+    if (node === null) {
+      return null;
+    } else if (data < node.data) {
+      node.left = this.removeNode(node.left, data);
+      return node;
+    } else if (data > node.data) {
+      node.right = this.removeNode(node.right, data);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if(node.right === null) {
+        node = node.left;
+        return node;
+      }
+      let newNode = this.minNode(node.right);
+      node.data = newNode.data;
+      node.right = this.removeNode(node.right, newNode.data);
+      return node;
+    }
+  }
+  minNode(node) {
+    if (node.left === null)
+      return node;
+    else
+      return this.minNode(node.left);
+  }
   min() {
     if (this.base === null) {
       return undefined
